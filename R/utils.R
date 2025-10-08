8100cf13ee1e7079303a5c9c2850465ab2d6d605
Akira Terui <hanabi0111@gmail.com>
2025-10-08 14:47:51 -0400

    📝 add description

--- R/utils.R
+++ R/utils.R
@@ -26,3 +26,68 @@ lag_base <- function(x, k = 1L) {
   else c(tail(x, k), rep(NA, -k))
 }
 
+#' Generate Lagged Values Within Groups
+#'
+#' This function computes lagged values of a specified variable within each group
+#' defined by a grouping (index) column. It first orders data within each group by
+#' a time or sequence variable, then applies a one-step lag to the target variable.
+#' The resulting data frame includes a new column containing lagged values.
+#'
+#' @param data A data frame containing at least the grouping, time, and response variables.
+#' @param y Character string specifying the name of the response variable to be lagged.
+#'   Default is \code{"n"}.
+#' @param t Character string specifying the name of the time or sequence variable
+#'   used to order observations within each group. Default is \code{"t"}.
+#' @param index Character string specifying the grouping variable (e.g., species, site, ID).
+#'   Default is \code{"species"}.
+#'
+#' @return A data frame identical to \code{data} but with an additional column named
+#'   \code{<y>_lag}, containing the one-step lagged values of the response variable
+#'   computed within each group.
+#'
+#' @examples
+#' data <- data.frame(
+#'   species = rep(c("A", "B"), each = 4),
+#'   t = rep(1:4, 2),
+#'   n = c(5, 6, 7, 8, 2, 3, 4, 5)
+#' )
+#'
+#' lag_block(data)
+#'
+#' @author Akira Terui, \email{hanabi0111@gmail.com}
+#'
+#' @seealso \code{\link{lag_base}}, \code{\link{split}}
+#'
+#' @export
+
+lag_block <- function(data,
+                      y = "n",
+                      t = "t",
+                      index = NULL) {
+
+  if (is.null(index)) {
+
+    data <- data[order(data[[t]]), ]
+    data[[paste0(y, "_lag")]] <- lag_base(data[[y]], k = 1)
+    df_lag <- data
+
+    if (any(table(data[[t]]) > 1))
+      message("Detected multiple records for the same time point. If you intended to create blocks separately for different groups, please specify the `index` argument.")
+
+  } else {
+
+    list_lag <- split(x = data,
+                      f = data[[index]]) |>
+      lapply(function(d) {
+        d <- d[order(d[[t]]), ]  # sort by time column
+        d[[paste0(y, "_lag")]] <- lag_base(d[[y]], k = 1)
+        return(d)
+      })
+
+    df_lag <- do.call(rbind,
+                      c(list_lag, make.row.names = FALSE))
+
+  }
+
+  return(df_lag)
+}
